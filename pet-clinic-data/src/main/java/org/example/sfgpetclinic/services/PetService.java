8b98704b43b628bb93f8aaa0640823d95aa2d452
Andy Tuffill <andy.tuffill@gmail.com>
2018-10-18 17:27:12 +0100

    Implement interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/org/example/sfgpetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package org.example.sfgpetclinic.services;
+
+import org.example.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/org/example/sfgpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package org.example.sfgpetclinic.services;
+
+import org.example.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
