3f3ac51b6bb6f0949bad2e8641670df8a7d9ee53
Andy Tuffill <andy.tuffill@gmail.com>
2018-10-30 18:16:36 +0000

    Adding specialities to vets in bootstrap. Closes #34

--- pet-clinic-web/src/main/java/org/example/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/org/example/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,11 +1,9 @@
 package org.example.sfgpetclinic.bootstrap;
 
-import org.example.sfgpetclinic.model.Owner;
-import org.example.sfgpetclinic.model.Pet;
-import org.example.sfgpetclinic.model.PetType;
-import org.example.sfgpetclinic.model.Vet;
+import org.example.sfgpetclinic.model.*;
 import org.example.sfgpetclinic.services.OwnerService;
 import org.example.sfgpetclinic.services.PetTypeService;
+import org.example.sfgpetclinic.services.SpecialityService;
 import org.example.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -18,16 +16,27 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
     private final PetTypeService petTypeService;
+    private final SpecialityService specialityService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
+        this.specialityService = specialityService;
     }
+
 
     @Override
     public void run(String... args) throws Exception {
 
+        int count = petTypeService.findAll().size();
+
+        if (count==0){
+            loadData();
+        }
+    }
+
+    private void loadData() {
         PetType dog = new PetType();
         dog.setName("Dog");
         PetType savedDogPetType = petTypeService.save(dog);
@@ -36,6 +45,17 @@ public class DataLoader implements CommandLineRunner {
         cat.setName("Cat");
         PetType savedCatPetType = petTypeService.save(cat);
 
+        Speciality radiology = new Speciality();
+        radiology.setDescription("Radiology");
+        Speciality savedRadiology = specialityService.save(radiology);
+
+        Speciality surgery = new Speciality();
+        surgery.setDescription("Surgery");
+        Speciality savedSurgery = specialityService.save(surgery);
+
+        Speciality dentistry = new Speciality();
+        dentistry.setDescription("Dentistry");
+        Speciality savedDentistry = specialityService.save(dentistry);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
@@ -75,17 +95,18 @@ public class DataLoader implements CommandLineRunner {
         Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
+        vet1.getSpecialities().add(savedRadiology);
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
         vet2.setFirstName("Bob");
         vet2.setLastName("Roberts");
+        vet2.getSpecialities().add(savedSurgery);
+        vet2.getSpecialities().add(savedDentistry);
 
         vetService.save(vet2);
 
         System.out.println("Loaded vets.....");
-
-
     }
 }
