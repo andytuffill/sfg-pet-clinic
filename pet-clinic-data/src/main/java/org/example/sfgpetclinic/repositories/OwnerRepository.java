5b553930b877df82eaf6e98f492d8608631e9fcc
Andy Tuffill <andy.tuffill@gmail.com>
2018-11-12 22:05:32 +0000

    added spring data JPA service for users. Closes #40

---
+++ pet-clinic-data/src/main/java/org/example/sfgpetclinic/repositories/OwnerRepository.java
@@ -0,0 +1,10 @@
+package org.example.sfgpetclinic.repositories;
+
+import org.example.sfgpetclinic.model.Owner;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
+
+}
