4723cd3b978eca7ba21abbc7d0b5369f4402b4f6
Andy Tuffill <andy.tuffill@gmail.com>
2018-11-13 16:00:59 +0000

    Added project lombok to project, closes #51

--- pet-clinic-data/src/main/java/org/example/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/org/example/sfgpetclinic/model/BaseEntity.java
@@ -1,11 +1,17 @@
 package org.example.sfgpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @MappedSuperclass
 public class BaseEntity implements Serializable {
 
--- pet-clinic-data/src/main/java/org/example/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/org/example/sfgpetclinic/model/Speciality.java
@@ -1,10 +1,17 @@
 package org.example.sfgpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name = "speciality")
 public class Speciality extends BaseEntity {
 
